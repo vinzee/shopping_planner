33f641093d209791b09ddc5b3517d9c04ef848bf
Vineet Ahirkar <vinzee93@gmail.com>
2016-12-03 14:39:33 -0500

    consider radius in aggregation

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.post('/get_shortest_path', function(req, res, next) {
 	if(!_.isNaN(req.body.lng) && !_.isNaN(req.body.lat) && !_.isNaN(req.body.radius) && req.body['product_types[]'].length > 0){
 		ClosestRouteCalculator.find(req.body.lat, req.body.lng, req.body.radius, req.body['product_types[]'], function(data){
 			if (!_.isEmpty(data.err)) {
-		      	res.status(500).json(data.err);
+				res.status(404).json(data.err);
 		    } else {
 				res.status(200).json(data.coords);
 		    }
